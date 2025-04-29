40f7a0fb83aa82518d9c6c32ec7b7233c3a412f4
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-29 13:22:55 +0530

    Initial Commit

---
+++ IProtechs-UIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IProtechs-UIKIT
+//
+//  Created by Nimish Mothghare on 29/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IProtechs-UIKITTests/IProtechs_UIKITTests.swift
@@ -0,0 +1,17 @@
+//
+//  IProtechs_UIKITTests.swift
+//  IProtechs-UIKITTests
+//
+//  Created by Nimish Mothghare on 29/04/25.
+//
+
+import Testing
+@testable import IProtechs_UIKIT
+
+struct IProtechs_UIKITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
