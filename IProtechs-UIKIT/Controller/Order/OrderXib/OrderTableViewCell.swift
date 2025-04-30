700f496eac8889e2612a1f5651cd74d4ae17375e
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-30 09:50:10 +0530

    Xcoded Upadted Changes

---
+++ IProtechs-UIKIT/Controller/Order/OrderXib/OrderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OrderTableViewCell.swift
+//  IProtechs-UIKIT
+//
+//  Created by Nimish Mothghare on 30/04/25.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
